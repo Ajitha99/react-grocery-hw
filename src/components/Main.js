9d407be3ffe1275f8657f5c5636ab9bd90b1c2ef
ajitha99 <ajitha.anandapu@gmail.com>
2022-03-21 06:07:40 -0400

    remove buttons added

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,18 +5,16 @@ import Result from './Result';
 import lists from '../Data'
 
 function Main (){
-
+    //states
     const [allList, setAllLists] = useState(lists);
-    // const options = [true,false];
-
-    // const [submit, setSubmit] = useState(false);
-    const [item, setItem] = useState(" ");
-    const [brand, setBrand] = useState(" ");
-    const [units, setUnits] = useState(" ");
+    
+    const [item, setItem] = useState("");
+    const [brand, setBrand] = useState("");
+    const [units, setUnits] = useState("");
     const [quantity, setQuantity] = useState(0);
     const [purchased, setPurchased] = useState(false);
 
-
+    //
     function getItem(e){
         e.preventDefault();
         setItem(e.target.value);
@@ -33,14 +31,13 @@ function Main (){
         e.preventDefault();
         setQuantity(e.target.value);
     }
-    // function getPurchased(e){
-    //     e.preventDefault();
-    //     (e.target.value != undefined) ? setPurchased(e.target.value): setPurchased(false);
-    // }
+   
     function getPurchased(e){
         e.preventDefault();
         setPurchased(e.target.value);
     }
+
+    //adds new item to list
     const onClick = (e) => {
         e.preventDefault();
         const newItem = {
@@ -53,13 +50,14 @@ function Main (){
         const newList = [...allList, newItem];
         setAllLists(newList);
         // setSubmit(true);
-        setItem(" ");
-        setBrand(" ");
-        setUnits(" ");
-        setQuantity(" ");
-        setPurchased(" ");
+        setItem("");
+        setBrand("");
+        setUnits("");
+        setQuantity(0);
+        setPurchased(false);
 
     }
+    //shows the list of items purchased (isPurchased value is true)
     const handleByPurchased = (e) =>{
         e.preventDefault();
         const newGroceryList = [...allList];
@@ -69,6 +67,7 @@ function Main (){
         })
         setAllLists(listByPurchased);
     }
+    //shows the list of items not purchased ( isPurchased value is false)
     const handleByNotPurchased = (e) =>{
         e.preventDefault();
         const newList = [...allList];
@@ -78,12 +77,13 @@ function Main (){
         })
         setAllLists(listByNotPurchased);
     }
+    //sorts list by item- alphabetically 
     const handleSortAlphabetically = (e) =>{
         e.preventDefault();
         console.log('sorting - clicked')
         const newGrocerList = [...allList];
         const sortListByItem = newGrocerList.sort((a,b) =>{
-            let aTemp = a.item.atoLocaleLowerCase();
+            let aTemp = a.item.toLocaleLowerCase();
             let bTemp = b.item.toLocaleLowerCase();
             if (aTemp < bTemp){
                 return -1;
@@ -97,7 +97,23 @@ function Main (){
         setAllLists(sortListByItem);
 
     }
-            
+    //sorts items by quantity
+    const handleSortByQuantity = (e) =>{
+        e.preventDefault();
+        console.log('sorting by quantity clicked')
+        const newList1 = [...allList];
+        const sortByQuantity = newList1.sort((a,b) =>{
+            return a.quantity - b.quantity;
+        })
+        setAllLists(sortByQuantity);
+    }
+    //deletes the item from list
+    const deleteEvent = (index) =>{
+        const newList2 = [...allList];
+        newList2.splice(index,1);
+        setAllLists(newList2);
+    }
+       
 
 
     return(
@@ -120,6 +136,7 @@ function Main (){
         <div><button id = "showPurchased" onClick={handleByPurchased}>Purchased</button>
             <button id = "showNotPurchased" onClick={handleByNotPurchased}>Not Purchased</button>
             <button id = "showAlphabetical" onClick = {handleSortAlphabetically}>Sort by Item</button>
+            <button id = "showQuantity" onClick = {handleSortByQuantity}>Sort by Quantity</button>
         </div>
         { 
         allList.map((listElement, index) =>{
@@ -132,9 +149,9 @@ function Main (){
                     units = {listElement.units}
                     quantity = {listElement.quantity}
                     isPurchased = {listElement.isPurchased}
+                    delete = {deleteEvent.bind(index)} //removes-button in Result prop
                     key = {index}
                     />
-                {/* <p onClick={()=>handleToggle(index,listElement.isPurchased)}>Remove</p> */}
                 </div>
             )
             })}
@@ -146,33 +163,3 @@ function Main (){
 
 export default Main;
 
-
-// return(
-//     <div>
-//       <div className="mainDiv">
-//         <label>Item: </label>
-//         <input id = "item" onChange={getItem} type="text" />
-//         <label>Brand: </label>
-//         <input id = "brand" onChange={getBrand} type="text" />
-//         <label>Units: </label>
-//         <input id = "units" onChange={getUnits} type="text" />
-//         <label>Quantity: </label>
-//         <input id = "quantity" onChange={getQuantity} type="text" />
-//         <label>Purchased: </label>
-//         <select id ="purchased" onChange={getPurchased} name="select">
-//             <option value= "true">true</option>
-//             <option value= "false">false</option>
-//         </select>
-//         <button onClick={onClick} type="button"> Submit</button>
-        
-//       </div>
-//       <div id = "result">{
-//           submit ?  <Result 
-//                  item = {item}
-//                  brand = {brand}
-//                  units = {units}
-//                  quantity = {quantity}
-//                  isPurchased = {purchased} /> : null }
-//         </div> 
-//     </div>
-// )
--- src/components/Result.js
+++ src/components/Result.js
@@ -3,36 +3,36 @@
 import { useState } from "react";
 
 function Result (props){
+    // states
     const [state , setState] = useState(false);
 
+    //when purchased in list is clicked - state changes to true: when true - toggles remove button.
     const handleTogglePurchase = (e) =>{
         e.preventDefault();
-        console.log("purchase clicked");
-        console.log(props.isPurchased);
+        // console.log("purchase clicked");
+        // console.log(props.isPurchased);
         setState(true);
             
     }
    
     return(
-        <div className=" topResultDiv">
-            <div className = "resultDiv">
+       <div className=" topResultDiv">
+            <div className = "resultDiv"> 
                 {/* {console.log("Iam in Result")} */}
-                {console.log(props)}
+                {/* {console.log(props)} */}
                 <h4><span>Item:</span>{props.item}</h4>
                 <h4><span>Brand:</span>{props.brand}</h4>
                 <h4><span>Units:</span>{props.units}</h4>
                 <h4><span>Quantity:</span> {props.quantity}</h4>
-                <div><h4 onClick={handleTogglePurchase}><span>Purchased:</span>{String(props.isPurchased)}</h4>
+                <div><h4  onClick={handleTogglePurchase}><span>Purchased:</span>{String(props.isPurchased)}</h4>
+                  {/* here, if state is true - toggling remove button and sending delete props to button */}
+                    {(state)? <button id = "removeBtn" onClick={props.delete}>remove</button>: null}
                   
-                  {(state)? <button id = "removeBtn">remove</button>: null}
-                  </div>
+                </div>
             </div>  
-        </div>
+         </div>
     )
 }
 
 export default Result;
 
-<button id = "removeBtn">remove</button> 
-   {/* <h4><span>Purchased: </span>{''+ props.isPurchased}</h4> */}
- {/* <h4><span>Purchased: </span>{JSON.stringify(props.isPurchased)}</h4> */}
